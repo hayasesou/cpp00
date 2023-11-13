491e19f333fce1d82d9730ed6cc2fb57de4aad22
Hayase Fukushi <hfukushi@c1r27s4.42tokyo.jp>
2023-11-13 18:31:46 +0900

    prototype of Contact.hpp us  using class Contact

---
+++ ex01/include/Contact.hpp
@@ -0,0 +1,17 @@
+#ifndef CONTACT_HPP
+#define  CONTACT_HPP
+
+
+#include <string>
+
+class CONTACT {
+	private	:
+		std::string 	first_name;
+		std::string 	last_name;
+		std::string 	nick_name;
+		//unsigned int	phone_mumber;
+		std::string		darkest_secret;
+	
+};
+
+#endif
---
+++ ex01/src/test.cpp
@@ -0,0 +1,9 @@
+#include "Contact.hpp"
+#include <iostream>
+
+int main()
+{
+	std::cout <<"hello";
+	return (0);
+}
+
