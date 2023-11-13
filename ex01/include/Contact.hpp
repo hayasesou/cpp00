05aa3b9d7be0b4b5adb42a0a197fef8e5c535534
Hayase Fukushi <hfukushi@c1r27s4.42tokyo.jp>
2023-11-13 20:23:02 +0900

    make func treating private member of class Contact by accesser

--- ex01/include/Contact.hpp
+++ ex01/include/Contact.hpp
@@ -2,16 +2,31 @@
 #define  CONTACT_HPP
 
 
+#include <iostream>
 #include <string>
 
 class Contact {
-	public	:
+	private	:
 		std::string 	first_name;
 		std::string 	last_name;
 		std::string 	nick_name;
-		//unsigned int	phone_mumber;
+		unsigned int	phone_number;
 		std::string		darkest_secret;
 	
+	public	:
+		void	set_contact(std::string f_name, std::string l_name, std::string n_name, unsigned int p_number,  std::string d_secret)
+		{
+			first_name =  f_name;
+			last_name = l_name;
+			nick_name = n_name;
+			phone_number = p_number;
+			darkest_secret = d_secret;
+		}
+		
+		void	print_contact(Contact contact)
+		{
+			std::cout << contact.first_name << std:: endl;
+		}
 };
 
 #endif
--- ex01/src/test.cpp
+++ ex01/src/test.cpp
@@ -5,9 +5,11 @@ int main()
 {
 	Contact	test;
 
-	test.first_name = "fukushi";
-	std::cout << test.first_name << std::endl;
-	std::cout << test.last_name << std::endl;
+	test.set_contact("jonathan", "josetar", "jojo", 123456789, "Dio killed my father");
+	//test.first_name = "fukushi";
+	//std::cout << test.first_name << std::endl;
+	//std::cout << test.last_name << std::endl;
+	test.print_contact(test);
 	return (0);
 }
 
